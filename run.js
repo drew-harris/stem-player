75d0660b35921822eac6358eb1f5f8c83f817bde
Drew Harris <61394759+drew-harris@users.noreply.github.com>
2021-12-28 22:25:08 -0600

    Fixed function name and small bugs

--- run.js
+++ run.js
@@ -8,10 +8,137 @@ runButton.addEventListener("click", async () => {
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: upload,
+    function: uploadLinks,
   });
 });
 
+let fileButton = document.getElementById("file-button");
+fileButton.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: files,
+  });
+});
+
+////////////////////////////////////////////////////////////////////////////////
+//                               File Upload
+////////////////////////////////////////////////////////////////////////////////
+async function files() {
+  async function uploadFiles() {
+    function waitForUpload(target) {
+      return new Promise((resolve) => {
+        observer = new MutationObserver(() => {
+          var newDoc = document.querySelector(
+            "#main > div > div.track-manager > div > div > form > button:nth-child(3) > span"
+          );
+          if (newDoc.innerText === "UPLOADED") {
+            observer.disconnect();
+            resolve("Done");
+          }
+        });
+        observer.observe(target, {
+          attributes: true,
+          childList: true,
+          subtree: true,
+          characterData: true,
+          characterDataOldValue: true,
+        });
+      });
+    }
+    var files = myFileForm.files;
+    var songs = [];
+    var songIndex = 0;
+
+    var track = [];
+    for (var i = files.length - 1; i >= 0; i--) {
+      if (files[i].type.includes("audio")) {
+        track.push(files[i]);
+        if (track.length === 4) {
+          songs.push(track);
+          track = [];
+        }
+      }
+    }
+    myFileForm.style.visibility = "hidden";
+    submitFolderButton.style.visibility = "hidden";
+
+    // Upload all songs
+    for (var i = 0; i < songs.length; i++) {
+      console.log(songs[i]);
+      extDisplay.innerHTML = "Uploading " + (i + 1) + " of " + songs.length;
+      songDisplay.innerHTML = songs[i][3].name;
+      var fileData = new DataTransfer();
+      for (var j = 0; j < songs[i].length; j++) {
+        fileData.items.add(songs[i][j]);
+      }
+      var fileInput = document.querySelector("#file");
+      fileInput.files = fileData.files;
+      fileInput.dispatchEvent(new Event("input", { bubbles: true }));
+      fileInput.dispatchEvent(new Event("change", { bubbles: true }));
+      var form = document.querySelector(
+        "#main > div > div.track-manager > div > div > form"
+      );
+      form.requestSubmit();
+      await waitForUpload(form);
+    }
+    extDisplay.innerHTML = "Uploaded " + songs.length + " songs";
+    songDisplay.innerHTML = "";
+  }
+  let myStuff = document.createElement("div");
+  myStuff.style.marginBottom = "40px";
+  let myFileForm = document.createElement("input");
+  myFileForm.setAttribute("directory", "");
+  myFileForm.setAttribute("webkitdirectory", "");
+  let selectFolderButton = document.createElement("div");
+
+  let submitFolderButton = document.createElement("div");
+  selectFolderButton.innerHTML = "[SELECT FOLDER]";
+  selectFolderButton.style.textAlign = "center";
+  selectFolderButton.style.marginBottom = "20px";
+  submitFolderButton.style.textAlign = "center";
+  selectFolderButton.style.cursor = "pointer";
+  submitFolderButton.style.cursor = "pointer";
+  submitFolderButton.style.visibility = "hidden";
+  submitFolderButton.innerHTML = "[UPLOAD ALBUM]";
+  selectFolderButton.addEventListener("click", () => {
+    myFileForm.click();
+  });
+  myFileForm.addEventListener("change", () => {
+    console.log(myFileForm.files);
+    selectFolderButton.innerHTML =
+      myFileForm.files[0].webkitRelativePath.split("/")[0];
+    submitFolderButton.style.visibility = "visible";
+  });
+  submitFolderButton.addEventListener("click", uploadFiles);
+  myStuff.appendChild(selectFolderButton);
+  myStuff.appendChild(submitFolderButton);
+  myFileForm.type = "file";
+  var extDisplay = document.createElement("div");
+  var songDisplay = document.createElement("div");
+  extDisplay.innerHTML = "";
+  extDisplay.style.textAlign = "center";
+  extDisplay.style.margin = "20px";
+  extDisplay.style.textDecoration = "underline";
+  songDisplay.innerHTML = "";
+  songDisplay.style.textAlign = "center";
+  songDisplay.style.margin = "20px";
+  myStuff.appendChild(extDisplay);
+  myStuff.appendChild(songDisplay);
+  let bigGroup = document.querySelector(
+    "#main > div > div.track-manager > div > div"
+  );
+  bigGroup.insertBefore(myStuff, bigGroup.children[1]);
+  try {
+  } catch (err) {
+    alert(err.message);
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//                               Link Upload
+////////////////////////////////////////////////////////////////////////////////
 async function uploadLinks() {
   try {
     function retrieveLinks() {
